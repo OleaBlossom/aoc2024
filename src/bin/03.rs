516dcb4cd5cb6965e59bb3036e41dd216bf48f50
olea <olea.plum@gmail.com>
2024-12-03 20:16:21 +0100

    day 3

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,11 +1,33 @@
+use regex::Regex;
+
 advent_of_code::solution!(3);
 
-pub fn part_one(input: &str) -> Option<i32> {
-    None
+pub fn part_one(input: &str) -> Option<i64> {
+    let re: Regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+
+    let mut v = vec![];
+    for (_, [x, y]) in re.captures_iter(input).map(|c| c.extract()) {
+        v.push(x.parse::<i64>().unwrap() * y.parse::<i64>().unwrap());
+    }
+
+    Some(v.iter().sum())
 }
 
-pub fn part_two(input: &str) -> Option<i32> {
-    None
+pub fn part_two(input: &str) -> Option<i64> {
+    let re: Regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    let mut v = vec![];
+
+    let lines: Vec<&str> = input.split("do()").collect();
+
+    for (_, line) in lines.iter().enumerate() {
+        let l: Vec<&str> = line.split("don't").collect();
+
+        for (_, [x, y]) in re.captures_iter(l.get(0).unwrap()).map(|c| c.extract()) {
+            v.push(x.parse::<i64>().unwrap() * y.parse::<i64>().unwrap());
+        }
+    }
+
+    Some(v.iter().sum())
 }
 
 #[cfg(test)]
@@ -21,6 +43,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(48));
     }
 }
