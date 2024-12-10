48649b94c5b5c1a2172b27d60933bed14bfeddbc
olea <olea.plum@gmail.com>
2024-12-10 22:03:30 +0100

    coding style

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -1,12 +1,12 @@
 advent_of_code::solution!(9);
 
-pub fn part_one(input: &str) -> Option<i32> {
+pub fn part_one(input: &str) -> Option<u64> {
     let mut result: Vec<String> = Vec::new();
-    let mut total: i32 = 0;
+    let mut total: u64 = 0;
     let mut pos: i32 = 0;
     for i in 0..input.len() {
-        let is_even = i.rem_euclid(2) == 1;
-        let n = input.get(i..i + 1).unwrap().parse::<i32>().unwrap();
+        let is_even: bool = i.rem_euclid(2) == 1;
+        let n: i32 = input.get(i..i + 1).unwrap().parse::<i32>().unwrap();
 
         let mut count = n;
         while count > 0 {
@@ -26,8 +26,6 @@ pub fn part_one(input: &str) -> Option<i32> {
     let count_dots: Vec<String> = result.clone();
     let count = count_dots.iter().filter(|&x| x.eq(".")).count();
 
-    println!("{:?}", result.join(""));
-
     let mut pos = result.len() - 1;
     for i in 0..result.len() - count {
         if result[i].eq(".") {
@@ -39,12 +37,11 @@ pub fn part_one(input: &str) -> Option<i32> {
         }
 
         if !result[i].eq(".") {
-            let r: i32 = result[i].parse::<i32>().unwrap() * i as i32;
+            let r: u64 = result[i].parse::<u64>().unwrap() * i as u64;
             total += r;
         }
     }
 
-    println!("{:?}", result.join(""));
     Some(total)
 }
 
